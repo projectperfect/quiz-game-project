d9e49ebccefd408815db3433c38378cbe227e40c
projectperfect <jprfct@protonmail.com>
2022-08-10 22:11:09 -0500

    quizbrain bug fixed.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -9,4 +10,5 @@ for question in question_data:
     question_bank.append(new_question)
 
 
-QuizBrain(quesiton_bank)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain():
         self.question_number = 0
         self.question_list = q_list
 
-    def next_question():
+    def next_question(self):
         current_question = self.question_list[self.question_number]
+        self.question_number += 1
         input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
